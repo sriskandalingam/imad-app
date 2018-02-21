8db4217a53b2c3051e23249294e662e5af9cc7ad
sriskandalingam <30955891+sriskandalingam@users.noreply.github.com>
2018-02-21 09:57:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/homepage',function(req,res){
+    res.send('home page is loading...');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
